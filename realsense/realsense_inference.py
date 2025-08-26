b4f17aa0c8b2205bc74965b9cd1ed3517df86894
Joel-Milla <jAlejandro.milla@outlook.com>
2025-08-26 14:38:27 -0600

    Change script to another folder.

--- realsense/realsense_inference.py
+++ realsense/realsense_inference.py
@@ -7,8 +7,8 @@ from laneatt import LaneATT
 
 MODEL_TO_LOAD = 'laneatt_100.pt' # Model name to load
 CONFIG_TO_LOAD = 'laneatt.yaml' # Configuration file name to load
-MODEL_PATH = os.path.join(os.path.dirname(__file__), 'checkpoints', MODEL_TO_LOAD) # Model path (In this case, the model is in the same directory as the script)
-CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'configs', CONFIG_TO_LOAD) # Configuration file path (In this case, the configuration file is in the same directory as the script)
+MODEL_PATH = os.path.join(os.path.dirname(__file__), '..', 'checkpoints', MODEL_TO_LOAD) # Model path (In this case, the model is in the same directory as the script)
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), '..', 'configs', CONFIG_TO_LOAD) # Configuration file path (In this case, the configuration file is in the same directory as the script)
 
 if __name__ == '__main__':
     laneatt = LaneATT(CONFIG_PATH)  # Creates the model based on a configuration file
